f2e38a6b0ea0b081a11357bbf072b6afa793a252
Programador36 <programador.analista36@ciudadmaderas.com>
2023-11-21 08:58:54 -0600

    Cambios al dia 21/11

--- application/controllers/Usuario.php
+++ application/controllers/Usuario.php
@@ -6,15 +6,35 @@ class Usuario extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->database('default');
-		$this->load->model('usuariosModel');
-		$this->load->model('generalModel');
-		
 		header('Access-Control-Allow-Origin: *');
 		header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 		header('Access-Control-Allow-Headers: Content-Type');
 
 		date_default_timezone_set('America/Mexico_City');
+
+		$urls = array('192.168.30.128/auth/jwt/login','localhost','http://localhost','http://localhost:3030','http://192.168.30.128/auth/jwt/login','192.168.30.128','http://192.168.30.128:3030','127.0.0.1','https://rh.gphsis.com','rh.gphsis.com','https://maderascrm.gphsis.com','maderascrm.gphsis.com');
+        date_default_timezone_set('America/Mexico_City');
+
+        if(isset($this->input->request_headers()['origin']))
+            $origin = $this->input->request_headers()['origin'];
+        else if(array_key_exists('HTTP_ORIGIN',$_SERVER))
+            $origin = $_SERVER['HTTP_ORIGIN'];
+        else if(array_key_exists('HTTP_PREFERER',$_SERVER))
+            $origin = $_SERVER['HTTP_PREFERER'];
+        else
+            $origin = $_SERVER['HTTP_HOST'];
+
+        if(in_array($origin,$urls) || strpos($origin,"192.168")) {
+			$this->load->database('default');
+			$this->load->model('usuariosModel');
+			$this->load->model('generalModel');
+
+            $this->load->helper(array('form','funciones'));
+			$this->load->library(array('session'));
+        } else {
+            die ("Access Denied");     
+            exit;  
+        }
 	}
 	public function index()
 	{
@@ -51,6 +71,55 @@ class Usuario extends CI_Controller {
 		$this->output->set_content_type("application/json");
         $this->output->set_output(json_encode($data));
 	}
+
+	public function insertBatchUsers()
+    {
+        $json = file_get_contents('php://input');
+        $params = json_decode($json, true);
+
+        $table = $params['nombreTabla'];
+        $data = $params['data'];
+    
+        $response['result'] = isset($table, $data) && !empty($data);
+        if ($response['result']) {
+            $fecha = date('Y-m-d H:i:s');
+            $complemento = date('Ymd');
+            $rows = array();
+            foreach ($data as $col) {
+                $iniciales = getIniciales($col['nombre']); // 
+                $row = array(
+                    'numContrato' => isset($col['nombre']) ? $iniciales.$complemento : null,
+                    'numEmpleado' => isset($col['nombre']) ? $iniciales.$complemento : null,
+                    'nombre' => isset($col['nombre']) ? $col['nombre'] : null,
+                    'telPersonal' => isset($col['telPersonal']) ? $col['telPersonal'] : null,
+                    'area' => isset($col['area']) ? $col['area'] : null,
+					'puesto' => isset($col['puesto']) ? $col['puesto'] : null,
+                    'oficina' => isset($col['oficina']) ? $col['oficina'] : null,
+                    'sede' => isset($col['sede']) ? $col['sede'] : null,
+                    'correo' => isset($col['correo']) ? $col['correo'] : null,
+                    'password' => isset($col['password']) ? encriptar($col['password']) : encriptar('Tempo01@'),
+                    'estatus' => 1,
+					'creadoPor' => 1,
+                    'fechaCreacion' => $fecha,
+                    'modificadoPor' => 1,
+                    'fechaModificacion' => $fecha,
+                );
+                $rows[] = $row;
+            }
+        
+            $response['result'] = $this->generalModel->insertBatch($table, $rows);
+            
+            if ($response['result']) {
+                $response['msg'] = "¡Listado insertado exitosamente!";
+            } else {
+                $response['msg'] = "¡No se ha podido insertar los datos!";
+            }
+        } else {
+            $response['msg'] = "¡Parametros invalidos!";
+        }
+        $this->output->set_content_type("application/json");
+        $this->output->set_output(json_encode($response));
+    }
 
 	public function updateUser() {
 		$fecha = date('Y-m-d H:i:s');
--- application/models/usuariosModel.php
+++ application/models/usuariosModel.php
@@ -19,7 +19,7 @@ class usuariosModel extends CI_Model {
 		$query = $this->db->get('usuarios'); 
 		return $query->result();
 	}
-	
+
 	public function login($numEmpleado,$password)
 	{
 		$query = $this->db->query("SELECT *  FROM usuarios WHERE numEmpleado='$numEmpleado' AND password='$password'");
@@ -29,16 +29,6 @@ class usuariosModel extends CI_Model {
 	public function getAreas()
 	{
 		$query = $this->db->distinct()->select('area')->get('usuarios');
-        return $query->result();
-	}
-
-	public function disableUser()
-	{
-		$this->db->set('estatus', 0); // Establece el valor del campo estatus a 0
-		$this->db->where('id_usuario', $idUsuario); // Especifica la condición WHERE para el usuario que deseas actualizar
-		$this->db->update('usuarios'); // Nombre de tu tabla de usuarios
-		$this->db->query();
-		
         return $query->result();
 	}
 }
