bfd39ef33a68cb1b4b94b8d63b8e7cc9bae4f6a1
Aureaac <84527420+Aureaac@users.noreply.github.com>
2024-01-05 15:44:48 -0600

    mis cambios

--- application/controllers/loginController.php
+++ application/controllers/loginController.php
@@ -48,27 +48,25 @@ class loginController extends CI_Controller {
 	}
 	public function addRegistroEmpleado(){
 		$this->db->trans_begin();
-		$datosEmpleado = json_decode(file_get_contents('php://input'));
-		$datosEmpleado = $datosEmpleado->params;
+		$datosEmpleado = $this->input->post('params');
 		$insertData = array(
-			"numContrato" => $datosEmpleado->idcontrato,
-			"numEmpleado" => $datosEmpleado->num_empleado,
-			"nombre" => $datosEmpleado->nombre_completo,
-			"telPersonal" => $datosEmpleado->tel_personal,
-			"telOficina" => $datosEmpleado->nom_oficina,
-			"area" => $datosEmpleado->area,
-			"puesto" => $datosEmpleado->puesto,
-			"oficina" => $datosEmpleado->nom_oficina,
-			"sede" => $datosEmpleado->sede,
-			"correo" => $datosEmpleado->email_empresarial,
-			"password" => encriptar($datosEmpleado->password),
+			"numContrato" => $datosEmpleado['idcontrato'],
+			"numEmpleado" => $datosEmpleado['num_empleado'],
+			"nombre" => $datosEmpleado['nombre_completo'],
+			"telPersonal" => $datosEmpleado['tel_personal'],
+			"telOficina" => $datosEmpleado['nom_oficina'],
+			"puesto" => $datosEmpleado['puesto'],
+			"oficina" => $datosEmpleado['nom_oficina'],
+			"sede" => $datosEmpleado['sede'],
+			"correo" => $datosEmpleado['email_empresarial'],
+			"password" => encriptar($datosEmpleado['password'] ),
 			"estatus" => 1,
 			"creadoPor" => 0,
 			"fechaCreacion" => date('Y-m-d H:i:s'),
 			"modificadoPor" => 0,
 			"fechaModificacion" => date('Y-m-d H:i:s')
 		);
-		$resultado = $this->generalModel->agregarRegistro('usuarios',$insertData);
+		$resultado = $this->generalModel->addRecord('usuarios',$insertData);
 		if ($this->db->trans_status() === FALSE){
             $this->db->trans_rollback();
 			if(strpos($resultado['message'], "UNIQUE")){
