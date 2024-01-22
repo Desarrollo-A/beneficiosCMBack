e7d926d6426191cce7fb1d140ffd5c290152a30a
rimtzg-maderas <programador.analista40@ciudadmaderas.com>
2024-01-22 17:40:53 -0600

    Bugfixes

--- application/models/MenuModel.php
+++ application/models/MenuModel.php
@@ -8,58 +8,51 @@ class MenuModel extends CI_Model {
 
     public function getMenu($id_user, $id_rol)
     {
-        $headers = $this->db->query("SELECT * FROM menu_headers")->result_array();
-        
-        $sections = array();
-        foreach ($headers as $key_h => $header) {
-            $items = $this->db->query("SELECT * FROM menu WHERE header=$header[id] AND father IS NULL")->result_array();
+        $subheader = array();
 
-            $menus = array();
-            foreach ($items as $key_i => $item) {
-                $children = $this->db->query("SELECT * FROM menu WHERE father=$item[id]")->result_array();
+        $items = $this->db->query("SELECT * FROM menu WHERE father IS NULL")->result_array();
 
-                $childs = array();
-                foreach ($children as $key_c => $child) {
+        $menus = array();
+        foreach ($items as $key_i => $item) {
+            $children = $this->db->query("SELECT * FROM menu WHERE father=$item[id]")->result_array();
 
-                    $roles_child = explode(',', $child['rol']);
-                    $users_can_child = explode(',', $child['users_can']);
-                    $users_not_child = explode(',', $child['users_not']);
+            $childs = array();
+            foreach ($children as $key_c => $child) {
 
-                    if(in_array($id_rol, $roles_child)){
-                        if(!in_array($id_user, $users_not_child)){
-                            array_push($childs, $child);
-                        }
-                    }elseif(in_array($id_user, $users_can_child)){
+                $roles_child = explode(',', $child['rol']);
+                $users_can_child = explode(',', $child['users_can']);
+                $users_not_child = explode(',', $child['users_not']);
+
+                if(in_array($id_rol, $roles_child)){
+                    if(!in_array($id_user, $users_not_child)){
                         array_push($childs, $child);
                     }
-                }
-
-                //IF
-                if($childs){
-                    $item["children"] = $childs;
-                }
-
-                $roles_menu = explode(',', $item['rol']);
-                $users_can_menu = explode(',', $item['users_can']);
-                $users_not_menu = explode(',', $item['users_not']);
-
-                if(in_array($id_rol, $roles_menu)){
-                    if(!in_array($id_user, $users_not_menu)){
-                        array_push($menus, $item);
-                    }
-                }elseif(in_array($id_user, $users_can_menu)){
-                    array_push($menus, $item);
+                }elseif(in_array($id_user, $users_can_child)){
+                    array_push($childs, $child);
                 }
             }
 
-            if($menus){
-                $header["items"] = $menus;
+            //IF
+            if($childs){
+                $item["children"] = $childs;
+            }
 
-                array_push($sections, $header);
+            $roles_menu = explode(',', $item['rol']);
+            $users_can_menu = explode(',', $item['users_can']);
+            $users_not_menu = explode(',', $item['users_not']);
+
+            if(in_array($id_rol, $roles_menu)){
+                if(!in_array($id_user, $users_not_menu)){
+                    array_push($menus, $item);
+                }
+            }elseif(in_array($id_user, $users_can_menu)){
+                array_push($menus, $item);
             }
         }
 
-        return $sections;
+        $subheader['items'] = $menus;
+
+        return array($subheader);
     }
 
 }
