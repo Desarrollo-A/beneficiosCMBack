f1dffe8bb86e584104a024f35dea30990b2e49fe
tino1243 <programador.analista32@ciudadmaderas.com>
2024-03-27 17:54:19 -0600

    cambios extraviados

--- application/controllers/EncuestasController.php
+++ application/controllers/EncuestasController.php
@@ -8,7 +8,9 @@ class EncuestasController extends BaseController {
 	public function __construct()
 	{
 		parent::__construct();
-		$this->ch = $this->load->database('ch', TRUE);
+		$this->schema_cm = $this->config->item('schema_cm');
+        $this->schema_ch = $this->config->item('schema_ch');
+        $this->ch = $this->load->database('ch', TRUE);
 		$this->load->database('default');
 		$this->load->model('EncuestasModel');
 		$this->load->model('GeneralModel');
@@ -99,7 +101,7 @@ class EncuestasController extends BaseController {
 		$area= $this->input->post('dataValue[area]');
 
 		$query_idEncuesta = $this->ch->query("SELECT * 
-		FROM PRUEBA_beneficiosCM.encuestascreadas WHERE idArea = $area AND estatus = 1");
+		FROM ". $this->schema_cm .".encuestascreadas WHERE idArea = $area AND estatus = 1");
 
         $idEnc = 0;
         foreach ($query_idEncuesta->result() as $row) {
@@ -110,13 +112,13 @@ class EncuestasController extends BaseController {
 			"estatus" => 0,
 		);
 
-		$this->GeneralModel->updateRecord('PRUEBA_beneficiosCM.encuestascreadas', $data_1, 'idEncuesta', $idEnc);
+		$this->GeneralModel->updateRecord('encuestascreadas', $data_1, 'idEncuesta', $idEnc);
 
 		$data_2 = array(
 			"estatus" => $estatus
 		);
 
-		$this->GeneralModel->updateRecord('PRUEBA_beneficiosCM.encuestascreadas', $data_2, 'idEncuesta', $idEncuesta);
+		$this->GeneralModel->updateRecord('encuestascreadas', $data_2, 'idEncuesta', $idEncuesta);
 		$this->ch->trans_complete();
 				
 		if ($this->ch->trans_status() === FALSE) {
@@ -135,7 +137,7 @@ class EncuestasController extends BaseController {
 			"diasVigencia" => $vigencia
 		);
 
-		$response=$this->GeneralModel->updateRecord('encuestasCreadas', $data, 'idEncuesta', $idEncuesta);
+		$this->GeneralModel->updateRecord('encuestasCreadas', $data, 'idEncuesta', $idEncuesta);
 		echo json_encode(array("estatus" => true, "msj" => "Dato Actualizado!" ), JSON_NUMERIC_CHECK);
 				
 	}
