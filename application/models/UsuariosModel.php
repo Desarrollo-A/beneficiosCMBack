9806e5b00b038575d25bb69ab63139ee39417fc5
tino1243 <programador.analista32@ciudadmaderas.com>
2024-05-29 13:27:28 -0600

    correos temporales para cualquier usuario

--- application/models/UsuariosModel.php
+++ application/models/UsuariosModel.php
@@ -42,8 +42,10 @@ class UsuariosModel extends CI_Model {
         us2.narea as 'area', us2.iddepto AS 'idDepto', us2.ndepto as 'departamento', us.idAreaBeneficio, 
         us.estatus, us.creadoPor, us.fechaCreacion, us.modificadoPor, us.fechaModificacion,
 		CASE
-			WHEN us2.mail_emp = '' THEN c.correo
-			ELSE us2.mail_emp
+			WHEN c.correo = '' THEN us2.mail_emp
+			WHEN c.correo IS NULL THEN us2.mail_emp
+			WHEN c.estatus = 0 THEN us2.mail_emp
+			ELSE c.correo
 		END AS correo 
         FROM ". $this->schema_cm .".usuarios AS us 
         INNER JOIN ". $this->schema_ch .".beneficioscm_vista_usuarios AS us2 ON us2.idcontrato = us.idContrato
@@ -74,18 +76,19 @@ class UsuariosModel extends CI_Model {
 			"SELECT US.*, us2.idsede AS idSede, us2.idArea, us2.tipo_puesto AS tipoPuesto, us2.fingreso AS fechaIngreso,
 			CONCAT(CONCAT (us2.nombre_persona,' ',us2.pri_apellido,' ',us2.sec_apellido),' ', '(', us2.nsede, ')') AS nombreCompleto, us2.npuesto as nombrePuesto, us2.tipo_puesto, 
 			CASE
-			WHEN us2.mail_emp = '' THEN c.correo
-			ELSE us2.mail_emp
+				WHEN c.correo = '' THEN us2.mail_emp
+				WHEN c.correo IS NULL  THEN us2.mail_emp
+				ELSE c.correo
 			END AS correo 
 			FROM ". $this->schema_cm .".usuarios US 
 			INNER JOIN ". $this->schema_ch .".beneficioscm_vista_usuarios us2 ON us2.idcontrato = US.idContrato 
+			LEFT JOIN PRUEBA_beneficiosCM.correostemporales AS c ON c.idContrato = us2.idcontrato 
 			WHERE US.idRol = ? AND US.estatus = ? AND us2.idsede IN ( SELECT DISTINCT idSede FROM ". $this->schema_cm .".atencionxsede WHERE idEspecialista = ? ) 
 			UNION ( SELECT u.idUsuario AS idUsuario, u.idContrato, u.password, us2.idRol, u.externo, u.idAreaBeneficio, us2.estatus, us2.creadoPor, us2.fechaCreacion,
 			 us2.modificadoPor, us2.fechaModificacion, 1 AS idSede, 0 AS idarea, 0 tipoPuesto, 0 AS fechaIngreso, CONCAT('(Lamat)', ' ', CONCAT(IFNULL(us2.nombre, ''))) AS nombreCompleto,
 			0 AS nombrePuesto, 0 AS tipo_puesto, us2.correo
 			FROM ". $this->schema_cm .".usuarios as u 
 			INNER JOIN ". $this->schema_cm .".usuariosexternos AS us2 ON us2.idContrato = u.idContrato 
-			LEFT JOIN ". $this->schema_cm .".correostemporales AS c ON c.idContrato = us2.idContrato 
 			WHERE u.externo = ? )",
 			 array( 2, 1, $idEspecialista, 1 )
 		);
@@ -169,6 +172,21 @@ class UsuariosModel extends CI_Model {
 
 	public function getUserByNumEmp($numEmpleado)
     {
+		$TempMail = $this->ch->query(
+			"SELECT * from PRUEBA_CH.beneficioscm_vista_usuarios us
+			INNER JOIN PRUEBA_beneficiosCM.correostemporales c ON c.idContrato = us.idcontrato 
+			WHERE us.num_empleado = ?", array( $numEmpleado ));
+
+		if ($TempMail->num_rows() > 0) {
+
+		$query = $this->ch->query(
+			"SELECT *, c.correo AS correo
+					FROM ". $this->schema_ch .".beneficioscm_vista_usuarios AS us
+					LEFT JOIN ". $this->schema_cm .".correostemporales c ON c.idContrato = us.idcontrato 
+					WHERE us.num_empleado = ?", array( $numEmpleado ));
+
+		}else{
+
         $query = $this->ch->query(
         "SELECT *,
 			CASE
@@ -178,19 +196,14 @@ class UsuariosModel extends CI_Model {
 			FROM ". $this->schema_ch .".beneficioscm_vista_usuarios AS us
 			LEFT JOIN ". $this->schema_cm .".correostemporales c ON c.idContrato = us.idcontrato 
 			WHERE us.num_empleado = ?", array( $numEmpleado ));
+		}
+		
         return $query;
     }
 
 	public function insertTempMail($correo, $idContrato)
     {
-		$query = $this->ch->query("SELECT * FROM ". $this->schema_cm .".correostemporales WHERE correo = ?", array($correo));
-
-        if ($query->num_rows() > 0) {
-			echo json_encode(array("estatus" => true, "msj" => "Código correcto" ), JSON_NUMERIC_CHECK); 
-			$this->ch->query("INSERT INTO table_name (correo, idContrato)
-			VALUES (?, ?);", array($correo, $idContrato));
-		}else{
-			echo json_encode(array("estatus" => false, "msj" => "El código insertado no es correcto"), JSON_NUMERIC_CHECK);
-		}
+		$this->ch->query("INSERT INTO ". $this->schema_cm .".correostemporales (correo, idContrato)
+		VALUES (?, ?);", array($correo, $idContrato));
     }
 }
