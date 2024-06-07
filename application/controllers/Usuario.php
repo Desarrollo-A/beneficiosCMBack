52a7dd6827202b9dabf9026571a7edd141346544
tino1243 <programador.analista32@ciudadmaderas.com>
2024-06-07 16:34:29 -0600

    c

--- application/controllers/Usuario.php
+++ application/controllers/Usuario.php
@@ -567,7 +567,7 @@ class Usuario extends BaseController {
 			-- LEFT JOIN ". $this->schema_cm .".correostemporales ct ON ct.idContrato = us.idcontrato 
 			WHERE us.mail_emp = '$correo' -- OR ct.correo = '$correo'");
 
-			if ($query_mailexist->num_rows() > 0 && ) {
+			if ($query_mailexist->num_rows() > 0) {
 				echo json_encode(array("estatus" => false, "msj" => "El correo ingresado ya está en uso"), JSON_NUMERIC_CHECK);
 			}else{
 
