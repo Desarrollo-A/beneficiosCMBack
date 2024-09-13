2808501bb9c82155c38508611d35a88156a967af
Programador36 <programador.analista36@ciudadmaderas.com>
2024-09-13 16:46:20 +0100

    Cambio de rama por revisión justificacion

--- application/controllers/perfil/EventosController.php
+++ application/controllers/perfil/EventosController.php
@@ -16,7 +16,6 @@ class EventosController extends BaseController {
 	}
 
     public function getEventos(){
-
         $idContrato = $this->input->post('dataValue[idContrato]');
         $idSede = $this->input->post('dataValue[idSede]');
         $idDepto = $this->input->post('dataValue[idDepartamento]');
@@ -35,61 +34,142 @@ class EventosController extends BaseController {
         $this->output->set_output(json_encode($rs, JSON_NUMERIC_CHECK));
 	}
 
-    public function nuevoEvento(){
-        $titulo            = $this->form('titulo');
-        $descripcion       = $this->form('descripcion');
+    // public function nuevoEvento(){
+    //     $titulo            = $this->form('titulo');
+    //     $descripcion       = $this->form('descripcion');
+    //     $fechaEvento       = $this->form('fechaEvento');
+    //     $inicioPublicacion = $this->form('inicioPublicacion');
+    //     $finPublicacion    = $this->form('finPublicacion');
+    //     $limiteRecepcion   = $this->form('limiteRecepcion');
+    //     $ubicacion         = $this->form('ubicacion');
+    //     $imagen            = $this->file('imagen');
+    //     $idUsuario         = $this->form('idUsuario');
+
+    //     // var_dump($imagen);
+    //     $fecha = date("Y-m-d H:i:s");
+
+    //     $upload = false;
+    //     if($imagen){
+    //         $file_ext = pathinfo($imagen->name, PATHINFO_EXTENSION);
+    //         // $file_name =  "Evento_".$titulo."_".$fechaEvento.".".$file_ext."";
+    //         $file_name =  "Evento_$titulo.$file_ext";
+    //         // $file_name =  "justificacion-$idCita.$file_ext";
+
+    //         $upload = $this->upload($imagen->tmp_name, $file_name);
+            
+    //         if($upload){
+    //             $evento['imagen'] = $file_name;
+    //         }
+    //     }
+        
+    //     $evento = [
+    //         "titulo" => $titulo,
+	// 		"descripcion" => $descripcion,
+	// 		"fechaEvento" => $fechaEvento,
+    //         "inicioPublicacion" => $inicioPublicacion,
+    //         "finPublicacion" => $finPublicacion,
+    //         "limiteRecepcion" => $limiteRecepcion,
+    //         "ubicacion" => $ubicacion,
+    //         "imagen" => $file_name,
+	// 		"creadoPor" => $idUsuario,
+	// 		"modificadoPor" => $idUsuario,
+	// 		"fechaModificacion" => $fecha,
+	// 		"fechaCreacion" => $fecha,
+	// 	];
+
+    //     var_dump($evento['imagen']); exit; die;
+
+    //     if($upload){
+    //         $rs["result"] = $this->GeneralModel->addRecord( $this->schema_cm.".eventos", $evento);
+    //     }else {
+    //         $rs["result"] = false;
+    //     }
+
+	// 	if ($rs["result"]) {
+	// 		$response["msg"] = "Se ha creado el evento";
+	// 	} else {
+	// 		$rs["msg"] = "No se ha podído crear el evento";
+	// 	}
+
+	// 	$this->output->set_content_type("application/json");
+    //     $this->output->set_output(json_encode($rs, JSON_NUMERIC_CHECK));
+	// }
+
+    public function nuevoEvento() {
+        $titulo            = trim($this->form('titulo'));
+        $descripcion       = trim($this->form('descripcion'));
         $fechaEvento       = $this->form('fechaEvento');
         $inicioPublicacion = $this->form('inicioPublicacion');
         $finPublicacion    = $this->form('finPublicacion');
         $limiteRecepcion   = $this->form('limiteRecepcion');
-        $ubicacion         = $this->form('ubicacion');
+        $ubicacion         = trim($this->form('ubicacion'));
         $imagen            = $this->file('imagen');
-        $idUsuario         = $this->form('idUsuario');
-
-        // var_dump($imagen);
+        $idUsuario         = (int) $this->form('idUsuario');
+    
         $fecha = date("Y-m-d H:i:s");
-
-        $upload = false;
-        if($imagen){
-            $file_ext = pathinfo($imagen->name, PATHINFO_EXTENSION);
-            // $file_name =  "Evento_".$titulo."_".$fechaEvento.".".$file_ext."";
-            $file_name =  "Evento_$titulo.$file_ext";
-            // $file_name =  "justificacion-$idCita.$file_ext";
-
-            $upload = $this->upload($imagen->tmp_name, $file_name);
-
-            if($upload){
-                $evento['imagen'] = $file_name;
+    
+        // Preparar el array de evento
+        $evento = [
+            "titulo"            => $titulo,
+            "descripcion"       => $descripcion,
+            "fechaEvento"       => $fechaEvento,
+            "inicioPublicacion" => $inicioPublicacion,
+            "finPublicacion"    => $finPublicacion,
+            "limiteRecepcion"   => $limiteRecepcion,
+            "ubicacion"         => $ubicacion,
+            "creadoPor"         => $idUsuario,
+            "modificadoPor"     => $idUsuario,
+            "fechaModificacion" => $fecha,
+            "fechaCreacion"     => $fecha,
+        ];
+    
+        // Procesar imagen
+        if ($imagen) {
+            $file_ext = strtolower(pathinfo($imagen->name, PATHINFO_EXTENSION));
+            $valid_ext = ['jpg', 'jpeg', 'png', 'gif']; // Extensiones válidas
+            if (in_array($file_ext, $valid_ext)) {
+                $file_name = "Evento_" . uniqid() . ".$file_ext"; // Nombre único
+                $upload = $this->upload($imagen->tmp_name, $file_name);
+    
+                if ($upload) {
+                    $evento['imagen'] = $file_name; // Añadir la imagen al array si la carga es exitosa
+                } else {
+                    $rs["msg"] = "Error al subir la imagen.";
+                    $this->output->set_content_type("application/json");
+                    $this->output->set_output(json_encode($rs, JSON_NUMERIC_CHECK));
+                    return;
+                }
+            } else {
+                $rs["msg"] = "Formato de imagen no permitido.";
+                $this->output->set_content_type("application/json");
+                $this->output->set_output(json_encode($rs, JSON_NUMERIC_CHECK));
+                return;
             }
         }
-
-        $evento = [
-			"titulo" => $titulo,
-			"descripcion" => $descripcion,
-			"fechaEvento" => $fechaEvento,
-            "inicioPublicacion" => $inicioPublicacion,
-            "finPublicacion" => $finPublicacion,
-            "limiteRecepcion" => $limiteRecepcion,
-            "ubicacion" => $ubicacion,
-			"creadoPor" => $idUsuario,
-			"modificadoPor" => $idUsuario,
-			"fechaModificacion" => $fecha,
-			"fechaCreacion" => $fecha,
-		];
-
-        if($upload){
-            $rs["result"] = $this->GeneralModel->addRecord( $this->schema_cm.".eventos", $evento);
-        }else {
-            $rs["result"] = false;
+    
+        // Insertar el evento en la base de datos
+        $rs["result"] = $this->GeneralModel->addRecord($this->schema_cm . ".eventos", $evento);
+    
+        // Respuesta según el resultado
+        if ($rs["result"]) {
+            $rs["msg"] = "Se ha creado el evento.";
+        } else {
+            $rs["msg"] = "No se ha podido crear el evento.";
         }
+    
+        // Devolver la respuesta como JSON
+        $this->output->set_content_type("application/json");
+        $this->output->set_output(json_encode($rs, JSON_NUMERIC_CHECK));
+    }
 
-		if ($rs["result"]) {
-			$response["msg"] = "Se ha creado el evento";
-		} else {
-			$rs["msg"] = "No se ha podído crear el evento";
-		}
+    public function actualizarAsistencia() {
+        $idDepto = $this->input->post('dataValue[idUsuario]');
+        $idContrato = $this->input->post('dataValue[idContrato]');
+        $idEvento = $this->input->post('dataValue[idEvento]');
 
-		$this->output->set_content_type("application/json");
-        $this->output->set_output(json_encode($upload, JSON_NUMERIC_CHECK));
-	}
+        // Devolver la respuesta como JSON
+        $this->output->set_content_type("application/json");
+        $this->output->set_output(json_encode($rs, JSON_NUMERIC_CHECK));
+    }
+    
 }
--- application/models/perfil/EventosModel.php
+++ application/models/perfil/EventosModel.php
@@ -13,14 +13,17 @@ class EventosModel extends CI_Model {
 
 	public function getEventos($idContrato, $idSede, $idDepto){
         $query = $this->ch->query(
-			"SELECT *FROM ". $this->schema_cm .".eventos AS ev
-            LEFT JOIN ". $this->schema_cm .".asistenciasEventos AS asis ON asis.idEvento = ev.idEvento AND asis.idContrato = ?
-            LEFT JOIN ". $this->schema_cm .".alcanceEvento AS alc ON alc.idEvento = ev.idEvento
-            WHERE alc.idSede = ? 
-              AND alc.idDepartamento = ? 
-              AND ev.estatus = 1 
-              AND (asis.idContrato IS NULL OR asis.estatus = 1)
-              AND alc.estatus = 1;", array($idContrato, $idSede, $idDepto));
+			"SELECT ev.idEvento, ev.titulo, ev.descripcion, ev.fechaEvento, ev.inicioPublicacion, ev.finPublicacion, ev.imagen, ev.horaEvento, ev.limiteRecepcion, ev.ubicacion,
+            asis.idAsistenciaEv, asis.idContrato, asis.confirmacion, asis.asistencia, 
+            (SELECT COUNT(*) FROM PRUEBA_beneficiosCM.asistenciasEventos WHERE idEvento = ev.idEvento AND confirmacion = 1) AS confirmados,
+            (SELECT COUNT(*) FROM PRUEBA_beneficiosCM.asistenciasEventos WHERE idEvento = ev.idEvento AND asistencia = 1) AS asistidos,
+            ev.estatus, ev.creadoPor, ev.fechaCreacion, ev.modificadoPor, ev.fechaModificacion
+            FROM PRUEBA_beneficiosCM.eventos AS ev
+            LEFT JOIN PRUEBA_beneficiosCM.asistenciasEventos AS asis ON asis.idEvento = ev.idEvento AND asis.idContrato = ?
+            LEFT JOIN PRUEBA_beneficiosCM.alcanceEvento AS alc ON alc.idEvento = ev.idEvento
+            WHERE  ev.estatus = 1 
+            AND (asis.idContrato IS NULL OR asis.estatus = 1)
+            AND (NOW() > ev.inicioPublicacion AND NOW() < ev.finPublicacion);", array($idContrato));
        	return $query;
     }
 }
